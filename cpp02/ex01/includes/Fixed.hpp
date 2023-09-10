71a8b923b4a5219f5644c8fa43809aee7ce5e2f3
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-09-10 18:21:09 +0900

    change ex01

---
+++ cpp02/ex01/includes/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+#include <cmath>
+
+class	Fixed
+{
+	public:
+		Fixed();
+		Fixed(int const num);
+		Fixed(float const num);
+		Fixed(Fixed const &fixed);
+		~Fixed();
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		int		toInt(void) const;
+		float	toFloat(void) const;
+		Fixed	&operator=(Fixed const &fixed);
+	private:
+		int		value;
+		static int const	bits = 8;
+};
+		
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
