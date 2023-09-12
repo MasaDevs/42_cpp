d5f217b380ab9884ed3bb6065cee1b5dd68d1206
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-09-12 17:44:35 +0900

    change cpp00 ex00

--- cpp00/ex00/srcs/megaphone.cpp
+++ cpp00/ex00/srcs/megaphone.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
+char	toupper(char ch)
+{
+	if ('a' <= ch && ch <= 'z')
+			ch -= ('a' - 'A');
+	return (ch);
+}
+
 int main(int argc, char **argv)
 {
 	if(argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for(int i = 0; argv[1][i]; i++)
-			std::cout << static_cast<char> (std::toupper(argv[1][i]));
+		for(int i = 1; i < argc; i++)
+			for(int j = 0; argv[i][j]; j++)
+				std::cout << toupper(argv[i][j]);
 		std::cout << std::endl;
 	}
 }
-			
-		
-	
+
--- lab/test.cpp
+++ lab/test.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <cstddef>
 
-std::vector<int> *test(int N)
+class	Square
 {
-	std::vector<int> *T = new std::vector<int>(N, 0);
-	return (T);
-}
+	public:
+		int height;
+		int width;
+		Square(int height, int width) {this->height = height; this->width = width;}
+		friend std::ostream& operator<<(std::ostream& out, const Square& sqr) {out <<  "Heigit: " << sqr.height << " Width: " << sqr.width << std::endl; return (out);}
+	private:
+		int hello;
+		int hello2;
+};
+		
+class	Rect: public Square
+{
+	public:
+		Rect(int height, int width, int depth):Square(height, width) {this->depth = depth;}
+		friend std::ostream& operator<<(std::ostream& ost, const Rect& rect) {ost <<  "Depth: " << rect.width<< std::endl; return (ost);}
+	private:
+		int depth;
+		int	data;
+};
 
 int main() {
-	std::vector<int> *T = test(10);
-	std::cout << T[5] << std::endl;
+	Rect rect(15, 20, 25);
+	std::cout << rect << std::endl;
+	std::cout << "Size: " << sizeof(rect) << std::endl;
+	std::cout << offsetof(Square, height) << std::endl;
+	std::cout << offsetof(Square, width) << std::endl;
+	std::cout << offsetof(Rect, height) << std::endl;
+	std::cout << offsetof(Rect, width) << std::endl;
+
     return 0;
 }
 
