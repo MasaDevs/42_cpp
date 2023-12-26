9a5cba88f4c797780c341d150c0d5076633e1eb4
massahito <masadevs@protonmail.com>
2023-12-26 15:34:58 +0900

    ex00

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -2,7 +2,7 @@
 #define	ITER_HPP
 
 template<typename T, typename U, typename V>
-void	iter(T &arr, U length, void (*func)(V tmp))
+void	iter(T &arr, U length, void (*func)(V &tmp))
 {
 	for (U i = 0; i < length; i++)
 		(*func)(arr[i]);
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,9 +1,12 @@
 #include "iter.hpp"
+#include <climits>
 #include <iostream>
 
-void	print(int	&num)
+template <typename T>
+void	print(T &e)
 {
-	std::cout << num << std::endl;
+	std::cout << e << std::endl;
+
 }
 
 void	change_to_n(char &chr)
@@ -11,16 +14,14 @@ void	change_to_n(char &chr)
 	chr = 'n';
 }
 
-//int main()
-//{
-	//std::cout << "----------PRINT----------" << std::endl;
-	//int			num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-	//iter(num, 10, print);
-
-	//std::cout << "----------CONVERT----------" << std::endl;
-	//std::string	str = "masahito arai";
-	//std::cout << "before iter:	" << str << std::endl;
-	//iter(str, str.size(), change_to_n);
-	//std::cout << "after iter:	"<< str << std::endl;
-	//std::cout << "this program ends successfully." << std::endl;
-//}
+int main()
+{
+	std::cout << "----------PRINT----------" << std::endl;
+	int			num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+	iter(num, 10, print<int>);
+	std::string	str = "masahito arai";
+	std::cout << "before iter:	" << str << std::endl;
+	iter(str, str.size(), change_to_n);
+	std::cout << "after iter:	"<< str << std::endl;
+	std::cout << "this program ends successfully." << std::endl;
+}
